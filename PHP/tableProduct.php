770941922252a6b66ebbde60532759045c67e8f8
tainuth1 <tainuth1@gmail.com>
2024-03-14 18:43:01 +0700

    update search

--- PHP/tableProduct.php
+++ PHP/tableProduct.php
@@ -7,23 +7,6 @@
 
     if($result){
         
-        while($row = mysqli_fetch_assoc($result)){
-            echo '
-            <tr>
-
-                <td>'.$row['id'].'</td>
-                <td>'.$row['name'].'</td>
-                <td>'.$row['category'].'</td>
-                <td>'.$row['brand'].'</td>
-                <td>'.$row['price'].'</td>
-                <td class="d-flex gap-2">
-                    <button class="btn btn-outline-primary" id="open-update" data-bs-toggle="modal" data-bs-target="#staticBackdrop"><i class="fa-regular fa-pen-to-square"></i> Edit</button>
-                    <form method="post"  onsubmit="return confirm(\'Are you sure to delete this product [ '.$row['name'].' ]\')""> 
-                        <button class="btn btn-danger" type="submit" name="_delete" value="'.$row['id'].'"><i class="fa-solid fa-trash-can"></i> Delete</button>
-                    </form>
-                </td>
-            </tr>';
-        }
         if(isset($_GET['_search'])){
             $filterRecord = $_GET['_search'];
             $sql_search = "SELECT * FROM `products` WHERE CONCAT(`name`, `category`, `brand`) LIKE '%$filterRecord%'";
@@ -33,7 +16,7 @@
             if($search_result){
                 foreach($search_result as $s_row){
                     ?>
-                        <tr class="table-dark">
+                        <tr class="">
                             <td><?= $s_row['id'] ?></td>
                             <td><?= $s_row['name'] ?></td>
                             <td><?= $s_row['category'] ?></td>
@@ -48,6 +31,24 @@
                         </tr>
                     <?php
                 }
+            }
+        }else{
+            while($row = mysqli_fetch_assoc($result)){
+                ?>
+                    <tr>
+                        <td><?= $row['id'] ?></td>
+                        <td><?= $row['name'] ?></td>
+                        <td><?= $row['category'] ?></td>
+                        <td><?= $row['brand'] ?></td>
+                        <td><?= $row['price'] ?></td>
+                        <td class="d-flex gap-2">
+                            <button class="btn btn-outline-primary" id="open-update" data-bs-toggle="modal" data-bs-target="#staticBackdrop"><i class="fa-regular fa-pen-to-square"></i> Edit</button>
+                            <form method="post"  onsubmit="return confirm(\'Are you sure to delete this product [ '.$row['name'].' ]\')""> 
+                                <button class="btn btn-danger" type="submit" name="_delete" value="'.$row['id'].'"><i class="fa-solid fa-trash-can"></i> Delete</button>
+                            </form>
+                        </td>
+                    </tr>
+                <?php
             }
         }
 
